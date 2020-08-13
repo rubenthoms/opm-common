d789ea89afc8017d76b5d1001429e8799f44d890
Ruben Manuel Thoms <ruben.thoms@ceetronsolutions.com>
2020-08-13 14:13:21 +0200

    Working now?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ import os
 import sys
 import opm
 sys.path.insert(0, os.path.abspath(".")
-sys.path.insert(0, os.path.abspath(dirname(opm.__file__)))
+sys.path.insert(0, os.path.abspath(os.path.dirname(opm.__file__)))
 
 
 # -- Project information -----------------------------------------------------
